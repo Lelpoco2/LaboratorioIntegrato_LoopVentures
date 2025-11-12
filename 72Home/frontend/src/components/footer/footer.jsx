0dfbdd1567f2e5733e16f1796fe7a67130fdc424
Cosmin04729 <cosmin.grosu@edu-its.it>
2025-11-12 17:22:12 +0100

    footer modified

--- 72Home/frontend/src/components/footer/footer.jsx
+++ 72Home/frontend/src/components/footer/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer className="footer container" role="contentinfo">
             <div className="footer-up">
                 <div className="footer-logo-section">
-                    
+
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, id.</p>
                     <button className="footer-btn" aria-label="Partiamo">Partiamo </button>
                 </div>
@@ -34,19 +34,13 @@ const Footer = () => {
                 <div className="footer-links">
                     <h4>Contatti</h4>
                     <ul>
+                        <li>Via Roma 123, Milano, Italia</li>
                         <li><a href="mailto:contact@us.com">contact@us.com</a></li>
                         <li><a href="tel:+14146875892">+41 468 758 92</a></li>
                     </ul>
                 </div>
 
-                <div className="footer-links">
-                    <h4>Seguici</h4>
-                    <div className="social-icons" aria-label="Social media">
-                        <a href="https://www.facebook.com/?locale=it_IT" target="_blank" rel="noreferrer" aria-label="Facebook">f</a>
-                        <a href="https://www.instagram.com/?hl=it" target="_blank" rel="noreferrer" aria-label="Instagram">ig</a>
-                        <a href="https://twitter.com/?lang=it" target="_blank" rel="noreferrer" aria-label="Twitter">t</a>
-                    </div>
-                </div>
+
             </div>
 
             <div className="footer-down">
@@ -55,7 +49,20 @@ const Footer = () => {
                     <a href="#"> Termini e Condizioni</a>
                     <a href="#"> Privacy Policy</a>
                 </p>
-            </div>
+
+                <div className="social-icons" aria-label="Social media">
+
+                    <h4 >Seguici</h4>
+
+                    <a href="https://www.facebook.com/?locale=it_IT" target="_blank" rel="noreferrer" aria-label="Facebook">f</a>
+                    <a href="https://www.instagram.com/?hl=it" target="_blank" rel="noreferrer" aria-label="Instagram">ig</a>
+                    <a href="https://twitter.com/?lang=it" target="_blank" rel="noreferrer" aria-label="Twitter">t</a>
+
+                </div>
+
+
+            </div>  
+
         </footer>
     );
 };
