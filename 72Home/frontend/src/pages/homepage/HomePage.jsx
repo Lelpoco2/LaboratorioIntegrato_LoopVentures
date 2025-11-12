0ea233683fb1579bfbfd74df04b601a47b42e6c3
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2025-11-12 18:25:15 +0100

    started structuring homepage

---
+++ 72Home/frontend/src/pages/homepage/HomePage.jsx
@@ -0,0 +1,40 @@
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/footer";
+import "./HomePage.css"
+
+export default function Homepage() {
+    return (
+        <>
+            <Navbar />
+            <div className="homepage">
+                    
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {/* <Footer /> */}
+            </div>
+        </>
+    );
+}
