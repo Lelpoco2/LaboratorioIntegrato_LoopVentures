5a974f52af1d51c0e451660d9183565ef77ba69d
Lelpoco2 <a.divizia.tfi@gmail.com>
2025-11-13 19:37:09 +0100

    feat: Add CORS configuration to SecurityConfig for development

--- 72Home/backend/src/main/java/com/_Home/backend/security/SecurityConfig.java
+++ 72Home/backend/src/main/java/com/_Home/backend/security/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com._Home.backend.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -18,5 +23,21 @@ public class SecurityConfig {
                 .anyRequest().permitAll()
             );
         return http.build();           
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource () {
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Setting DEV 
+
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
