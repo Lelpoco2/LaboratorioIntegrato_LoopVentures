f51a5a1904d3937d1dca4bb4253716474da98a6f
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2025-11-13 11:42:12 +0100

    fixed overlay

--- 72Home/frontend/src/components/cta-button/BottoneCTA.jsx
+++ 72Home/frontend/src/components/cta-button/BottoneCTA.jsx
@@ -5,7 +5,7 @@ export default function BottoneCTA() {
     return (
         <>
             <button className="button">
-                Get in touch <ArrowRightIcon size={20} weight="bold" className="right-arrow" />
+                Valuta ora <ArrowRightIcon size={20} weight="bold" className="right-arrow" />
             </button>
         </>
     );
