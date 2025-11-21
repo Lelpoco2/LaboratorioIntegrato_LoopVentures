91167860e6b7c5c1b6775876f1110c494612e0f6
Lelpoco2 <a.divizia.tfi@gmail.com>
2025-11-21 17:59:34 +0100

    fix: Update TestController comments and add Swagger annotation for exclusion from API docs

--- 72Home/backend/src/main/java/com/_Home/backend/controllers/TestController.java
+++ 72Home/backend/src/main/java/com/_Home/backend/controllers/TestController.java
@@ -14,13 +14,17 @@ import com._Home.backend.models.User;
 import com._Home.backend.services.OmiZoneService;
 import com._Home.backend.services.PropertyEvaluationServiceImpl;
 import com._Home.backend.services.UserService;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-// Controller for testing various services
+// Controller for testing various services. It is not show in in the Swagger API documentation.
 
+@Hidden
 @RestController
 @RequestMapping("/test")
 public class TestController {
